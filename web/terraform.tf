5ef4e52ea81e246bdbd2fd79910ace170b4f199f
mmreddy1582 <35883559+mmreddy1582@users.noreply.github.com>
2021-10-04 22:32:17 +0800

    Update terraform.tf

--- web/terraform.tf
+++ web/terraform.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "azurerm" {
         resource_group_name  = "mura-remote-state-rg"
-        storage_account_name = "muraterraformremotestate"
+        storage_account_name = "muraterraformremotestat1"
         container_name       = "muratfstate"
         key                  = "muraweb.tfstate"
     }
